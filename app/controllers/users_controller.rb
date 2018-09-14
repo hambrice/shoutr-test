7d3fb17c428b2f6ee6655703316a7c163f4c76b5
github username <rbh0005@gmail.com>
2018-09-14 15:07:52 -0700

    made changes to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < Clearance::UsersController
 
-  def new
-    @user = User.new
-  end
-
   def show
     @user = User.find(params[:id])
     @shouts = @user.shouts
   end
+
+  def new
+    @user = User.new
+  end
+
 
   private
 
